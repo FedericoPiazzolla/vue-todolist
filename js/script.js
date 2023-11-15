23f796403a20ff13bfc6cd11e381c1ed6ba6b74c
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-15 17:45:05 +0100

    add: v-for in li elem, and create function for push input

--- js/script.js
+++ js/script.js
@@ -4,9 +4,19 @@ createApp({
   data() {
     return {
 
+      toDoListElem: {
+        text:"",
+        done: false,
+      },
+    
+      toDoList: [ ],
+
     }
   },
   methods: {
-
+    addGoal: function() {
+      this.toDoList.push({...this.toDoListElem});
+      this.toDoListElem.text = "";
+    },
   }
 }).mount("#app")
